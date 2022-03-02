d3bbbcf8ac85065613872ef818a3eff352456a0e
Luis Arocha <larochanvarro@newtech.coppellisd.com>
2022-03-02 15:17:14 -0600

    Update PerformanceTask.java

--- src/labs/PerformanceTask.java
+++ src/labs/PerformanceTask.java
@@ -1,11 +1,29 @@
 package labs;
 
+import java.util.Scanner;
+
 public class PerformanceTask {
 
 	public static void main(String[] args) {
+
+		Scanner inKey = new Scanner(System.in);
+		System.out.print("Enter launch velocity: ");
+		int velocity = inKey.nextInt();
+		System.out.print("Enter launch angle: ");
+		int angle = inKey.nextInt();
+		
+		int x = velocity;
+		int y = angle;
+		
+		double angle2 = Math.sqrt(angle);
+		Math.cos(Math.toRadians(angle));
+		Math.sin(Math.toRadians(angle));
 		
 		
 		
+		System.out.println("Time: 0s");
+		System.out.println("pos x: " + Math.cos(angle) * 3 * velocity);
+		System.out.println("pos y: " + Math.sin(angle) * 3 * velocity + 0.5 * 9.8 * angle2);
 		
 		
 		
